1201e22c2ff2e00c63d9d7558809f5ea74e09993
Theo Poette <theo.poette@kbrwadventure.comm>
2025-08-06 23:45:15 +0200

    feat: v2 with full provisionning of amazon EC2

--- test-ansible.sh
+++ test-ansible.sh
@@ -4,9 +4,9 @@ docker build -t ansible-test-image .
 
 docker run -d --name ansible-test-container -p 2222:22 ansible-test-image
 
-sleep 5
+sleep 1
 
-ansible-playbook -i inventory.ini setup.yaml
+ansible-playbook -i inventory.yaml setup.yaml
 
-# docker stop ansible-test-container
-# docker rm ansible-test-container
+docker stop ansible-test-container
+docker rm ansible-test-container
